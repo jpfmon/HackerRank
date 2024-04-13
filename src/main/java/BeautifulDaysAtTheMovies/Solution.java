da5a0492fcccc919e68094c53d42795099a8834e
Juan Pablo Fernandez Montojo <jpfmontojo@gmail.com>
2024-04-13 11:48:55 +0300

    Beautiful Days

---
+++ src/main/java/BeautifulDaysAtTheMovies/Solution.java
@@ -0,0 +1,68 @@
+package BeautifulDaysAtTheMovies;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k) {
+        // Write your code here
+        int count = 0;
+        for (int number = i; number <= j; number++){
+            if (checkEvenly(number, k)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean checkEvenly(int number, int k) {
+        return ((Math.abs((number) - getReverse(number))) % k == 0);
+    }
+
+    private static int getReverse(int number) {
+        return Integer.parseInt(new StringBuilder(number + "").reverse().toString());
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
